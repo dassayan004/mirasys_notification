d9072e90eb7b3272dea7e2e60b616f364716223e
Sayan <dassayan004@gmail.com>
2025-10-30 17:59:22 +0530

    feat: add Swagger documentation setup and enable CORS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,48 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigSchema } from './common/config/schema';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<ConfigSchema, true>);
 
   const port = configService.getOrThrow<number>('PORT');
   const baseUrl = configService.getOrThrow<string>('BASE_URL');
+  const env = configService.getOrThrow<string>('NODE_ENV');
+
+  // swagger openapi
+  const swaggerPrefix = 'swagger';
+  const config = new DocumentBuilder()
+    .setTitle('AI Api')
+    .setDescription('The AI API description')
+    .setVersion(env.toUpperCase())
+    .addServer(baseUrl)
+    .addBearerAuth()
+    .setExternalDoc('Postman Collection', `${swaggerPrefix}/json`)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(swaggerPrefix, app, document, {
+    jsonDocumentUrl: `${swaggerPrefix}/json`,
+  });
+
+  // Middleware
   app.enableCors({
     origin: '*',
     credentials: true,
   });
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.enableShutdownHooks();
+
+  // Pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(port);
   Logger.log(`🚀 Application is running on: ${baseUrl}`);
